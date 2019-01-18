f3816e5c02e20fb374cedfd86a8ab21ce538967b
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2019-01-18 23:38:03 +0100

    Step 3

---
+++ app/src/main/java/se/ju/fljo/todoapp/MainActivity.java
@@ -0,0 +1,10 @@
+package se.ju.fljo.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jonatan on 2019-01-18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}

1c469fc0f7adbb47c5455e5a14d01b89f710e7fd
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2019-01-22 14:14:05 +0100

    More progress.

---
+++ app/src/main/java/se/ju/fljo/todoapp/CreateActivity.java
@@ -0,0 +1,20 @@
+package se.ju.fljo.todoapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Jonatan on 2019-01-22.
+ */
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+
+}
--- app/src/main/java/se/ju/fljo/todoapp/MainActivity.java
+++ app/src/main/java/se/ju/fljo/todoapp/MainActivity.java
@@ -1,9 +1,11 @@
 package se.ju.fljo.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Jonatan on 2019-01-18.
@@ -15,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createButtonClicked(View view) {
+        System.out.println("Button clicked");
+        Intent intent = new Intent(this, CreateActivity.class);
+        startActivity(intent);
     }
 }
